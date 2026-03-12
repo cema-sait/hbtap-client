66fd8f064d9b2af81fcb3372fde13ad848ca5a60
Antony <antomutwiri2001@gmail.com>
2026-03-12 22:07:18 +0300

    updated the apis

--- app/api/public/index.ts
+++ app/api/public/index.ts
@@ -1,10 +1,10 @@
 import { PublicProposal, PublicProposalResponse } from "@/types/new/public";
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
-export const API_URL = `${process.env.API_URL || API_BASE_URL}`;
+// const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+// export const API_URL = `${process.env.API_URL || API_BASE_URL}`;
 
 
-// export const API_URL = "/api";
+export const API_URL = "/api";
 
 export async function getPublicProposals(): Promise<PublicProposal[]> {
 const res = await fetch(`${API_URL}/v3/proposals/`);
